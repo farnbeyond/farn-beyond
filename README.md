04e5001564568b3ababd2bbbb329a79be891b54c
Elvann <elvann.abendroth@gmail.com>
2025-02-03 12:33:27 +0200

    ⭐

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Far'n Beyond
+
+Live Site: https://farn-beyond.vercel.app/
+
+We’re an independent melodic metal band from Helsinki, Finland and is mixing 80’s synths with a female lyrical singer. The band was originally founded in 2002.
+
+We're currently in the process of recording new songs. Stay in touch by following us on Facebook & YouTube!
+
+## changeLog
+
+### 2025-02-01
+
+- UPDATE: Content update with new photoshoot.
+- UPDATE: Color scheme that fit with our new shoot.
+- FIXED: Footer styling
+-  TODO: For some reason the files are rendered statically on the live version of the site. 
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,8 +8,8 @@ export default async function Footer() {
   const footerText = footer.fields.text
 
   return (
-    <footer className=" bg-foreground italic items-center rounded-t-xl shadow-lg px-6 sm:px-8 py-16">
-      <div className="max-w-content w-content mx-auto">
+    <footer className="flex flex-col bg-foreground italic items-center rounded-t-xl shadow-lg px-6 sm:px-8 py-16">
+      <div className="flex flex-col max-w-content w-content mx-auto">
         <Markdown
           className="typo-small text-center text-faded prose prose-a:text-faded prose-a:hover:text-white"
           markdown={footerText}
